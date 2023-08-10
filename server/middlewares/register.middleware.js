11ccfaa993fc713a9b28624488c91ade28da6fad
Jean-Paul <jpzambrano.leon@gmail.com>
2023-08-10 17:56:55 -0500

    created register.middleware.js

---
+++ server/middlewares/register.middleware.js
@@ -0,0 +1,17 @@
+const User = require("../models/user.model");
+
+exports.checkDuplicateEmail = async (req, res, next) => {
+  if (req.body.email) {
+    let user = await User.findOne({
+      email: req.body.email,
+    });
+
+    if (user) {
+      return res.status(400).json({
+        error: true,
+        message: "Email is already in use",
+      });
+    }
+  }
+  next();
+};
