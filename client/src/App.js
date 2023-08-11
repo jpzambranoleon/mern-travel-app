737477fc18d968ab57546e5cf32c2858783f6c12
Jean-Paul <jpzambrano.leon@gmail.com>
2023-08-10 20:07:58 -0500

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,15 @@ import { Rating } from "@mui/material";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { logout } from "./redux/userSlice";
 import Card from "./components/Card";
-import mapboxgl from "!mapbox-gl";
+// added the following 6 lines.
+import mapboxgl from "mapbox-gl";
+
+// The following is required to stop "npm build" from transpiling mapbox code.
+// notice the exclamation point in the import.
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass =
+  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 function App() {
   const dispatch = useDispatch();
