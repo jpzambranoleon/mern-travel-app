e8d96b2ab9b9e0ac8e927a1363ef4dec47cd9e57
Jean-Paul <jpzambrano.leon@gmail.com>
2023-08-11 00:27:17 -0500

    modified file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import Map, { Marker, Popup } from "react-map-gl";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { Room } from "@mui/icons-material";
@@ -82,123 +82,120 @@ function App() {
   };
 
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <ReactMapGL
-        {...viewState}
-        mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
-        onMove={(e) => setViewState(e.viewState)}
-        onDblClick={currentUser && handleAddClick}
-        maxZoom={13}
-      >
-        {pins.map((p) => (
-          <>
-            <Marker
+    <Map
+      {...viewState}
+      mapLib={import("mapbox-gl")}
+      mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      onMove={(e) => setViewState(e.viewState)}
+      onDblClick={currentUser && handleAddClick}
+      style={{ height: "100vh" }}
+    >
+      {pins.map((p) => (
+        <>
+          <Marker
+            latitude={p.lat}
+            longitude={p.lng}
+            offsetLeft={-3.5 * viewState.zoom}
+            offsetTop={-7 * viewState.zoom}
+          >
+            <Room
+              style={{
+                fontSize: 7 * viewState.zoom,
+                color:
+                  currentUser?.username === p.username ? "tomato" : "slateblue",
+                cursor: "pointer",
+              }}
+              onClick={() => handleMarkerClick(p._id, p.lat, p.lng)}
+            />
+          </Marker>
+          {p._id === currentPlaceId && (
+            <Popup
+              key={p._id}
               latitude={p.lat}
               longitude={p.lng}
-              offsetLeft={-3.5 * viewState.zoom}
-              offsetTop={-7 * viewState.zoom}
-            >
-              <Room
-                style={{
-                  fontSize: 7 * viewState.zoom,
-                  color:
-                    currentUser?.username === p.username
-                      ? "tomato"
-                      : "slateblue",
-                  cursor: "pointer",
-                }}
-                onClick={() => handleMarkerClick(p._id, p.lat, p.lng)}
-              />
-            </Marker>
-            {p._id === currentPlaceId && (
-              <Popup
-                key={p._id}
-                latitude={p.lat}
-                longitude={p.lng}
-                closeButton={true}
-                closeOnClick={false}
-                onClose={() => setCurrentPlaceId(null)}
-                anchor="left"
-              >
-                <Card pin={p} setPins={setPins} />
-              </Popup>
-            )}
-          </>
-        ))}
-        {newPlace && (
-          <>
-            <Marker
-              latitude={newPlace.lat}
-              longitude={newPlace.lng}
-              offsetLeft={-3.5 * viewState.zoom}
-              offsetTop={-7 * viewState.zoom}
-            >
-              <Room
-                style={{
-                  fontSize: 7 * viewState.zoom,
-                  color: "tomato",
-                  cursor: "pointer",
-                }}
-              />
-            </Marker>
-            <Popup
-              latitude={newPlace.lat}
-              longitude={newPlace.lng}
               closeButton={true}
               closeOnClick={false}
-              onClose={() => setNewPlace(null)}
+              onClose={() => setCurrentPlaceId(null)}
               anchor="left"
             >
-              <div>
-                <form onSubmit={handleSubmit}>
-                  <label>Title</label>
-                  <input
-                    placeholder="Enter a title"
-                    autoFocus
-                    onChange={(e) => setTitle(e.target.value)}
-                  />
-                  <label>Description</label>
-                  <textarea
-                    placeholder="Say us something about this place."
-                    onChange={(e) => setDesc(e.target.value)}
-                  />
-                  <label>Rating</label>
-                  <Rating
-                    name="half-rating"
-                    defaultValue={0}
-                    precision={0.5}
-                    onChange={(e) => setRating(e.target.value)}
-                  />
-                  <button type="submit" className="submitButton">
-                    Add Pin
-                  </button>
-                </form>
-              </div>
+              <Card pin={p} setPins={setPins} />
             </Popup>
-          </>
-        )}
-        {currentUser ? (
-          <button className="button logout" onClick={handleLogout}>
-            Log out
+          )}
+        </>
+      ))}
+      {newPlace && (
+        <>
+          <Marker
+            latitude={newPlace.lat}
+            longitude={newPlace.lng}
+            offsetLeft={-3.5 * viewState.zoom}
+            offsetTop={-7 * viewState.zoom}
+          >
+            <Room
+              style={{
+                fontSize: 7 * viewState.zoom,
+                color: "tomato",
+                cursor: "pointer",
+              }}
+            />
+          </Marker>
+          <Popup
+            latitude={newPlace.lat}
+            longitude={newPlace.lng}
+            closeButton={true}
+            closeOnClick={false}
+            onClose={() => setNewPlace(null)}
+            anchor="left"
+          >
+            <div>
+              <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input
+                  placeholder="Enter a title"
+                  autoFocus
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Description</label>
+                <textarea
+                  placeholder="Say us something about this place."
+                  onChange={(e) => setDesc(e.target.value)}
+                />
+                <label>Rating</label>
+                <Rating
+                  name="half-rating"
+                  defaultValue={0}
+                  precision={0.5}
+                  onChange={(e) => setRating(e.target.value)}
+                />
+                <button type="submit" className="submitButton">
+                  Add Pin
+                </button>
+              </form>
+            </div>
+          </Popup>
+        </>
+      )}
+      {currentUser ? (
+        <button className="button logout" onClick={handleLogout}>
+          Log out
+        </button>
+      ) : (
+        <div className="buttons">
+          <button className="button login" onClick={() => setShowLogin(true)}>
+            Log in
           </button>
-        ) : (
-          <div className="buttons">
-            <button className="button login" onClick={() => setShowLogin(true)}>
-              Log in
-            </button>
-            <button
-              className="button register"
-              onClick={() => setShowRegister(true)}
-            >
-              Register
-            </button>
-          </div>
-        )}
-        {showRegister && <Register setShowRegister={setShowRegister} />}
-        {showLogin && <Login setShowLogin={setShowLogin} />}
-      </ReactMapGL>
-    </div>
+          <button
+            className="button register"
+            onClick={() => setShowRegister(true)}
+          >
+            Register
+          </button>
+        </div>
+      )}
+      {showRegister && <Register setShowRegister={setShowRegister} />}
+      {showLogin && <Login setShowLogin={setShowLogin} />}
+    </Map>
   );
 }
 
